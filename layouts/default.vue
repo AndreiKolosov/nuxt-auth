ad700582d8e4119abfce02e557c334bbeb61454d
AndreiKolosov <AndreiKolosov@users.noreply.github.com>
2024-01-27 10:34:05 +0200

    feat: bse layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -18,7 +18,47 @@ import { APP_ROUTES } from '~/utils/constants';
 </template>
 
 <style scoped>
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 40px;
+  min-height: 60px;
+  background: #033888;
+}
+.nav {
+  display: flex;
+  width: fit-content;
+  column-gap: 10px;
+}
 
+.nav__link {
+  text-decoration: none;
+  color: #fff;
+  transition: color 0.2s ease-in;
+  font-weight: 600;
+}
 
+.nav__link:hover {
+  color: #e00707;
+}
 
+.main {
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+}
+
+.header__loginLink {
+  font-weight: 600;
+  padding: 8px 15px;
+  background-color: #fff;
+  border-radius: 8px;
+  text-decoration: none;
+  color: #072fe0;
+  transition: transform 0.2s ease-in;
+}
+.header__loginLink:hover {
+  transform: scale(0.97);
+}
 </style>
